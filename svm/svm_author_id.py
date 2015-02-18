f8a9a7f4dfd6a0640d9ee620c823a1c35a261b2a
Minh Luong <en14vn@gmail.com>
2015-02-18 01:03:00 -0800

    Lesson 2 - SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,11 +21,27 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+## only use 1% of the training data set
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=10000.0)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, pred)
+
+print "Chris' (1) emails count:", sum(pred)
+print "Sarah's (0) emails count:", len(pred) - sum(pred)
 
 #########################################################
 
